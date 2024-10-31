01e45d65cac5f10f029fd30485793eb1f167702b
Sairaj Pawar <sairajepawar994@gmail.com>
2024-11-01 00:06:02 +0530

    completed backend

---
+++ backend/src/db/index.ts
@@ -0,0 +1,62 @@
+import {Client,Pool} from "pg";
+
+const config = require("../../data/config");
+
+const client = new Pool(config);
+
+function generateCodes():string{
+    const t:string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let code:string = "";
+    const size:number = t.length;
+    for (let i = 0; i < 10; i++) {
+        let index:number = Math.floor(Math.random()*size);
+        code += t[index];
+    }
+    return code;
+}
+
+async function createCodes():Promise<string>{
+    let code:string;
+    let flag:boolean = true;
+    do{
+        code = generateCodes();
+        const result = await client.query(`select COUNT(*) as count from master_table where code=$1`,[code]);
+        if(result.rows[0].count==0){
+            flag = false;
+        }
+    }while(flag)
+    return code;
+}
+
+async function createRoute(link: string): Promise<string>{
+    let code:string;
+    try {
+        await client.connect();
+        code = await createCodes();
+        await client.query(`insert into master_table(url,code) values ($1,$2)`,[link,code]);
+        return code;
+    }
+    catch (err: any) {
+        console.error(err)
+        return  "failed";
+    }
+
+}
+
+async function getLink(code:string): Promise<string> {
+    try{
+        await client.connect();
+        const result = await client.query(`select url from master_table where code=$1`,[code]);
+        let link:string = result.rows[0].url;
+        return link;
+    }
+    catch (err: any) {
+        console.error(err);
+        return "failed";
+    }
+}
+
+export {
+    createRoute,
+    getLink
+}
---
+++ backend/src/index.ts
@@ -0,0 +1,53 @@
+import express,{Request,Response} from "express";
+import {createRoute,getLink} from "./db"
+
+const app = express();
+app.use(express.json());
+
+app.get("/link", async (req: Request, res: Response) => {
+    const code:string = req.body.code;
+    try
+    {
+        const link: string = await getLink(code);
+        if(link=="failed"){
+            throw new Error("Code not present in Database/ Expired");
+        }
+        res.json({
+            link: link
+        })
+    }
+    catch(err:any){
+        res.status(500).json({
+            message: err.message,
+        })
+    }
+})
+
+app.post("/create", async (req: Request, res: Response) => {
+    const link:string = req.body.link;
+    try{
+        let code:string = await createRoute(link);
+        if(code==undefined){
+            throw new Error("Code generation failed");
+        }
+        res.json({
+            code: code,
+            message: "Successfully Done"
+        })
+    }
+    catch(err:any){
+        res.status(500).json({
+            message: err.message,
+        })
+    }
+})
+
+// for check whether backend is accessible or not
+app.get("/hello", (req:Request, res:Response):void => {
+    res.send("Hello World!");
+});
+
+
+app.listen(3000,():void=>{
+    console.log("Server started");
+});
